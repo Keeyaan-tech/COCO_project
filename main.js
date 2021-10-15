321a89f6f7fbb3e6d8facf675e865754b1b560fa
Keeyaan-tech <78153976+Keeyaan-tech@users.noreply.github.com>
2021-10-15 16:04:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function bedHtml(){
+    window.location = "bed.html";;
+}
+
+function tvHtml(){
+    window.location = "tv.html";;
+}
+
+function deskHtml(){
+    window.location = "laptop.html";;
+}
+
+function bottleHtml(){
+    window.location = "bottle.html";;
+}
+
+function fruitHtml(){
+    window.location = "banana.html";;
+}
